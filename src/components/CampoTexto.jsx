18f78e56664e972b2b4763f37aeb842e057b79d0
mateus roman <mroman6@ucs.br>
2026-09-11 18:38:38 -0300

    fix: clean initial

--- src/components/CampoTexto.jsx
+++
@@ -1,9 +0,0 @@
-export default function CampoTexto({ rotulo, dica, ...props }) {
-  return (
-    <label className="campo">
-      <span className="campo-rotulo">{rotulo}</span>
-      <input className="campo-input" {...props} />
-      {dica && <span className="campo-dica">{dica}</span>}
-    </label>
-  )
-}
--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home({ usuario, aoSair }) {
-  return (
-    <main className="home">
-      <h1>Ola, {usuario.nome}!</h1>
-      <p>Login funcionando. A lista de tarefas entra na proxima etapa.</p>
-      <button className="botao" onClick={aoSair}>Sair</button>
-    </main>
-  )
-}
